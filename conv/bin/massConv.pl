7ebe6b7eef0f9dc1bcb4dba248a24cabf281f694
vopl <vopl@bk.ru>
2016-10-04 14:55:08 +0300

    .

--- conv/bin/massConv.pl
+++ conv/bin/massConv.pl
@@ -11,10 +11,10 @@ use File::stat;
 use File::Basename;
 use File::Path qw(make_path);
 
-my $srcDir = '/root/share99/voxforge';
+my $srcDir = '/root/share99/voxforge/uncompressed';
 my $dstDir = '/root/share99/voxforge.out_10k_10x';
 
-my $cmdPrefix = '/root/share99/sp-conv-bin/sp-conv --ppw=1 --splp=100 --efmin=0.2 --efmax=20000 --efcount=10000 --eftype=flog --sfminmult=100 --sfmaxmult=1 --sfcountmult=10 --fps=1000 --inititersmax=10 --itersmax=1';
+my $cmdPrefix = './sp-conv --ppw=1 --splp=100 --efmin=0.2 --efmax=20000 --efcount=10000 --eftype=flog --sfminmult=100 --sfmaxmult=1 --sfcountmult=10 --fps=1000 --inititersmax=10 --itersmax=1';
 
 
 
@@ -50,6 +50,8 @@ for(;;)
 
         next if -f $outNameReady;# уже переработан
 
+        make_path(dirname($outName));
+
         if(-f $outName)
         {
             my $now = time;
@@ -60,16 +62,22 @@ for(;;)
             sleep 1;
             next if stat($outName)->mtime != $now;# кто то другой захватил
         }
-
-        make_path(dirname($outName));
+        else
+        {
+            open(my $file, '>>', $outName) || die "Couldn't touch $outName: $!";
+            close($file);
+        }
 
         my $cmd = "$cmdPrefix --in-file=$entry --out-file=$outName";
         print "exec $cmd\n";
 
         $workerPid = fork() or exec($cmd);
         waitpid($workerPid, 0);
-        my $ret = $? >> 8;
-        exit($ret) if $ret;
+        if($?)
+        {
+            warn "sp-conv failed: $?";
+            exit($?);
+        }
 
         open(my $file, '>>', $outNameReady) || die "Couldn't touch $outNameReady: $!";
         close($file);
