93acfd958f6edb57dbb1cf53996d25b75f806911
vopl <vopl@bk.ru>
2016-10-04 13:03:42 +0300

    .

--- conv/bin/massConv.pl
+++ conv/bin/massConv.pl
@@ -3,71 +3,78 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 use File::Find;
 use File::Spec;
 use File::stat;
 use File::Basename;
 use File::Path qw(make_path);
 
-my $srcDir = '/home/vopl/run/voxforge';
-my $dstDir = '/home/vopl/run/voxforge.out';
+my $srcDir = '/root/share99/voxforge';
+my $dstDir = '/root/share99/voxforge.out_10k_10x';
 
-my $cmdPrefix = '/home/vopl/projects/sp/conv/bin/sp-conv --ppw=1 --splp=100 --efmin=0.2 --efmax=20000 --efcount=1000 --eftype=flog --sfminmult=100 --sfmaxmult=1 --sfcountmult=1 --fps=1000 --inititersmax=1 --itersmax=1';
+my $cmdPrefix = '/root/share99/sp-conv-bin/sp-conv --ppw=1 --splp=100 --efmin=0.2 --efmax=20000 --efcount=10000 --eftype=flog --sfminmult=100 --sfmaxmult=1 --sfcountmult=10 --fps=1000 --inititersmax=10 --itersmax=1';
 
 
 
 $| = 1;
 
+my $stop = 0;
+my $workerPid;
+
+local $SIG{TERM} = \&signal_handler;
 local $SIG{INT} = \&signal_handler;
 sub signal_handler
 {
-    kill -9 -$$;
+    $stop = 1;
+    warn "kill $workerPid";
+    kill 'TERM', $workerPid;
     die "Caught a signal [".shift()."] [$!]";
 }
 
 mkdir $dstDir;
 
-
-
-sub entry()
-{
-    return unless -f $File::Find::name && $File::Find::name =~ /.wav$/;
-
-    my $outName = $dstDir.'/'.File::Spec->abs2rel($File::Find::name, $srcDir);
-    my $outNameReady = $outName;
-
-    $outName =~ s/.wav$/.spectr/;
-    $outNameReady =~ s/.wav$/.ready/;
-    
-    return if -f $outNameReady;# уже переработан
-    
-    if(-f $outName)
-    {
-        my $now = time;
-        my $mt = stat($outName)->mtime;
-        return if $now - $mt < 1*60; #был затронут менее часа назад, пропустить такой, вдруг с ним кто работает
-        
-        utime $now, $now, $outName;# не вполне четкая синхронизация, тронуть, секунду подождать и посмотреть
-        sleep 1;
-        return if stat($outName)->mtime != $now;# кто то другой захватил
-    }
-    
-    make_path(dirname($outName));
-    
-    my $cmd = "$cmdPrefix --in-file=$File::Find::name --out-file=$outName";
-    print "exec $cmd\n";
-    
-    my $ret = system($cmd);
-    exit($ret) if $ret;
-    
-    open(my $file, '>>', $outNameReady) || die "Couldn't touch $outNameReady: $!";
-    close($file);
-}
-
 for(;;)
 {
-    find(\&entry, $srcDir);
-    
+    my @entries;
+    find({wanted=>sub {push(@entries, $File::Find::name) if -f $File::Find::name && $File::Find::name =~ /.wav$/;}, no_chdir=>1}, $srcDir);
+
+    for my $entry (sort @entries)
+    {
+        my $outName = $dstDir.'/'.File::Spec->abs2rel($entry, $srcDir);
+        my $outNameReady = $outName;
+
+        $outName =~ s/.wav$/.spectr/;
+        $outNameReady =~ s/.wav$/.ready/;
+
+        next if -f $outNameReady;# уже переработан
+
+        if(-f $outName)
+        {
+            my $now = time;
+            my $mt = stat($outName)->mtime;
+            next if $now - $mt < 60*60; #был затронут менее часа назад, пропустить такой, вдруг с ним кто работает
+
+            utime $now, $now, $outName;# не вполне четкая синхронизация, тронуть, секунду подождать и посмотреть
+            sleep 1;
+            next if stat($outName)->mtime != $now;# кто то другой захватил
+        }
+
+        make_path(dirname($outName));
+
+        my $cmd = "$cmdPrefix --in-file=$entry --out-file=$outName";
+        print "exec $cmd\n";
+
+        $workerPid = fork() or exec($cmd);
+        waitpid($workerPid, 0);
+        my $ret = $? >> 8;
+        exit($ret) if $ret;
+
+        open(my $file, '>>', $outNameReady) || die "Couldn't touch $outNameReady: $!";
+        close($file);
+    }
+
     print "loop, now some sleep";
     sleep 10;
 }
